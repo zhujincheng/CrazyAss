bf1c9d1f0414d058011287b892578fb570ca2412
zhujincheng <452335710@qq.com>
2018-04-19 17:44:36 +0800

    add file

--- redis缓存.py
+++ redis缓存.py
@@ -5,10 +5,11 @@ import redis
 r = redis.Redis(host='192.168.0.125', port=6379,)
 r.set('foo', 'Bar')
 print(r.get('foo'))
+
 print(r.get('name'))
-
-print(r.set('hello') )
-
 """
-注释
+注释1
+"""
+"""
+注释2
 """
