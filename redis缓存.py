4c21b4b0830a97df40521d516366571be83307c2
zhujincheng <452335710@qq.com>
2018-04-19 18:00:20 +0800

    update

--- redis缓存.py
+++ redis缓存.py
@@ -5,11 +5,12 @@ import redis
 r = redis.Redis(host='192.168.0.125', port=6379,)
 r.set('foo', 'Bar')
 print(r.get('foo'))
-
-print(r.get('name'))
+print(r.get('name')
+        )
 """
 注释1
 """
+
 """
 注释2
 """
